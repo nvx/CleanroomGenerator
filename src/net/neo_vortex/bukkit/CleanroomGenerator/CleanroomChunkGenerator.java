b9a20b7292a87114cdb693abdee39da44367f9e4
NV <neovortex@gmail.com>
2011-07-10 05:00:39 +1000

    Added README and LICENSE. Also added copyright header to source files.

--- src/net/neo_vortex/bukkit/CleanroomGenerator/CleanroomChunkGenerator.java
+++ src/net/neo_vortex/bukkit/CleanroomGenerator/CleanroomChunkGenerator.java
@@ -1,7 +1,23 @@
+/*
+ * Cleanroom Generator
+ * Copyright (C) 2011 nvx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.neo_vortex.bukkit.CleanroomGenerator;
 
-import java.security.PrivateKey;
-import java.sql.Struct;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
